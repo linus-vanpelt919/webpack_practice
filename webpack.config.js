a5f008b090712e48aba187ca20d8a9259e1f783a
hiroshi sato <thelastwezardof@gmail.com>
2022-02-27 15:21:34 +0900

    Genelate HTML through Pug

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //cssをファイルに出力するためのもの
 const HtmlWebpackPlugin = require('html-webpack-plugin'); //htmlを出力するためのもの
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //distの中の不用なファイルの削除
-const { loader } = require('mini-css-extract-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin'); //distの中の不用なファイルの削除
+const {
+    loader
+} = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/js/main.js',
@@ -11,38 +15,55 @@ module.exports = {
         filename: 'js/main.js',
     },
     module: {
-        rules: [
-         {
-            test: /\.css/,
-            use: [//ローダーは下から適用される
-                {
-                  loader:MiniCssExtractPlugin.loader,
-                },
-                {
-                  loader: 'css-loader',
-                },
-             ]
-         },
-         {
-            test: /\.(jpe?g|png|svg|gif)$/i,
-            use: [
-                {
-                loader: 'file-loader',
-                options: {
-                    esModule: false,
-                    name: '[path][name].[ext]',
-                }
-                },
-            ]
-         }
-    ]
+        rules: [{
+                test: /\.css/,
+                use: [ //ローダーは下から適用される
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|svg|gif)$/i,
+                use: [//画像
+                    {
+                    loader: 'file-loader',
+                    options: {
+                        esModule: false,
+                        name: 'images/[name].[ext]',
+                    }
+                }, ]
+            },
+            {
+                test: /\.pug/,
+                use: [//テンプレートエンジン
+                    {
+                        loader: 'html-loader',
+                    },
+                    {
+                        loader: 'pug-html-loader',
+                        options: {
+                            pretty: true,
+                        }
+                    },
+                ]
+            }
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
             filename: './css/main.css',
         }),
         new HtmlWebpackPlugin({
-            template: './src/templates/index.html',
+            template: './src/templates/index.pug',
+            filename: 'index.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/access.pug',
+            filename: 'access.html',
         }),
         new CleanWebpackPlugin(),
     ],
